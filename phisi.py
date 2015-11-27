d7f686afe031ff11998a8bc40b699be531702b0b
Timoshuk_I <Timoshuk_I@fmsh2007>
2015-11-27 11:22:17 +0400

    Add phisi.py, plani.py

---
+++ phisi.py
@@ -0,0 +1,85 @@
+from plani import * #/KDqDmyKg
+
+class Hole:
+    def __init__(self, x, y, z, R):
+        self.sphere = Sphere(x, y, z, R)
+        
+    def score(self, ball):
+        if dist(self.sphere, ball.misticsphere) < 0:
+            ball.speed = Vector(0, 1000000, 0)
+            points -= 1
+            
+    
+class Ball:
+    def __init__(self, x, y, z, R, m, vx, vy, vz):
+        self.sphere = Sphere(x, y, z, R)
+        self.speed = Vector(vx, vy, vz)
+        self.Nspeed = Vector(vx, vy, vz)
+        self.misticsphere = Sphere(x + vx, y + vy, z + vz, R)
+        
+    def move(self):
+        self.speed = self.Nspeed
+        self.sphere.x += self.speed.x
+        self.sphere.y += self.speed.y
+        self.sphere.z += self.speed.z
+        self.misticsphere.x += self.speed.x
+        self.misticsphere.y += self.speed.y
+        self.misticsphere.z += self.speed.z
+        
+        
+    def dist(self, other):
+        return self.sphere.dist(other.sphere)
+    
+    def isInHole(self):
+        for hole in holes:
+            if dist(hole.sphere, self.misticsphere) < 0:
+                self.speed = Vector(0, 0, 0)
+                return True
+        else:
+            return False    
+    
+    def resistanse(self, resistanse):
+        self.speed.x = self.speed.x - self.speed.x * resistanse
+        self.speed.y = self.speed.y - self.speed.y * resistanse
+        self.speed.z = self.speed.z - self.speed.z * resistanse
+        
+    def hit(self, direction, forse):
+        self.speed += direction * forse / self.m    
+    
+    def wall(self, SIZE_X, SIZE_Y, SIZE_Z, wall_resistanse):
+        if self.misticsphere.x - self.sphere.R <= 0 or self.misticsphere.x + self.sphere.R >= SIZE_X:
+            self.speed.x = self.speed.x * (-1 + wall_resistanse)
+        if self.misticsphere.y - self.sphere.R <= 0 or self.misticsphere.y + self.sphere.R >= SIZE_Y:
+            self.speed.y = self.speed.y * (-1 + wall_resistanse)
+        if self.misticsphere.z - self.sphere.R <= 0 or self.misticsphere.z + self.sphere.R >= SIZE_Z:
+            self.speed.z = self.speed.z * (-1 + wall_resistanse)
+        
+    def crash(self, other):
+        self.Nspeed, other.Nspeed = bump(self.speed, other.speed, Vector(self.sphere.x - other.sphere.x, self.sphere.y - other.sphere.y, self.sphere.z - other.sphere.z))
+        
+
+class Box:
+    def __init__(self,  sizeX, sizeY, sizeZ, balls, holes, resistanse, wall_resistanse):
+        self.sizeX = sizeX
+        self.sizeY = sizeY
+        self.sizeZ = sizeZ
+        self.balls = balls
+        self.holes = holes
+        self.resistanse = resistanse
+        self.wall_resistanse = wall_resistanse
+
+    def movement():
+        points = len(self.balls)
+        for ball in self.balls:
+            ball.wall(self.sizeX, self.sizeY, self.sizeZ, wall_resistanse)
+        for ball in self.balls:
+            ball.resistanse(self.resistanse)
+        for ball in self.balls:
+            for hole in self.holes:
+                hole.score(ball)
+        for i in range(len(self.balls)):
+            for j in range(i + 1, len(self.balls)):
+                if self.balls[i].dist(balls[j]) <= 0:
+                    self.balls[i].crash(balls[j])
+        for ball in self.balls:
+            ball.move() 
---
+++ plani.py
@@ -0,0 +1,43 @@
+class Sphere:
+    def __init__(self, x, y, z, R):
+        self.x = x
+        self.y = y
+        self.z = z 
+        self.R = R
+        
+    def dist(self, other):
+        return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2 + (self.z - other.z) ** 2) ** 0.5 - self.R - other.R
+
+
+class Vector:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+   
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+   
+    def __mul__(self, other):
+        return Vector(self.x * other, self.y * other, self.z * other)
+   
+    def __rmul__(self, other):
+        return Vector(self.x * other, self.y * other, self.z + other)
+   
+    def __abs__(self):
+        return (self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5
+   
+
+def progection(self, other):
+    X = self.x
+    Y = self.y
+    Z = self.z
+    Xpro = X * (other.x / abs(other))
+    Ypro = Y * (other.y / abs(other))
+    Zpro = Z * (other.z / abs(other))
+    return (other * (1 / abs(other))) * (Xpro + Ypro + Zpro)
+
+def bump(first, second, line):
+    Fpro = progection(first, line)
+    Spro = progection(second, line)
+    return first + Fpro * (-1) + Spro, second + Spro * (-1) + Fpro
